53b22c92562cadfd146b9a0f2b5e64e4ade1d3e2
Priyansh Thakar <priyanshthakar@gmail.com>
2023-07-10 11:59:05 -0400

    Added GUI

---
+++ src/main/java/com/georgiancollege/week10/SearchController.java
@@ -0,0 +1,25 @@
+package com.georgiancollege.week10;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class SearchController {
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private ListView<Movie> listView;
+
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    void showMovies(ActionEvent event) {
+
+    }
+
+}
